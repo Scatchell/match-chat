012f9130771427b1976e4a1ff0d11ca344d9c4d2
Anthony Scatchell <Scatchell@gmail.com>
2014-03-10 21:08:38 +0200

    Changing db to postgres

--- Gemfile
+++ Gemfile
@@ -48,3 +48,4 @@ end
 gem 'private_pub'
 gem 'thin'
 gem 'devise'
+gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140306222454) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chatrooms", force: true do |t|
     t.string   "title"
     t.text     "description"
@@ -28,8 +31,8 @@ ActiveRecord::Schema.define(version: 20140306222454) do
     t.datetime "updated_at"
   end
 
-  add_index "messages", ["chatroom_id"], name: "index_messages_on_chatroom_id"
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
+  add_index "messages", ["chatroom_id"], name: "index_messages_on_chatroom_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 20140306222454) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
